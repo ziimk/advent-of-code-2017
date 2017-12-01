1e57fc70baa1d5c9ec9428316bc054b71e584156
Siim <siimkibus@gmail.com>
2017-12-01 23:27:41 +0200

    add day 1 and tests

---
+++ test/1-captcha.test.js
@@ -0,0 +1,29 @@
+const sequenceAdder = require('../1-captcha');
+
+describe('sequenceAdder()', () => {
+    describe('adds number if it matches the next one', () => {
+	    it('1122', () => {
+		    const input = '1122';
+
+		    expect(sequenceAdder(input)).toBe(3);
+	    });
+
+	    it('11122', () => {
+		    const input = '11122';
+		
+		    expect(sequenceAdder(input)).toBe(4);
+	    });
+
+	    it('1111', () => {
+		    const input = '1111';
+		
+		    expect(sequenceAdder(input)).toBe(4);
+	    });
+    });
+
+	it('does not add numbers from sequence if no matches', () => {
+		const input1 = '1234';
+
+		expect(sequenceAdder(input1)).toBe(0);
+	})
+});
