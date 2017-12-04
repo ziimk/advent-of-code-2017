51904bb8d430c12cc7c9f765233726305b45be32
Siim <siimkibus@gmail.com>
2017-12-04 22:56:53 +0200

    add day 3 part 1 solution

--- 3-path-in-spiral.js
+++ 3-path-in-spiral.js
@@ -3,9 +3,12 @@
  * spiraling outward. For example, the first few squares are allocated like this:
  * 17  16  15  14  13
  * 18   5   4   3  12
- * 19   6   1   2  11
+ * 19   6   1   2  11 <- Axis
  * 20   7   8   9  10
  * 21  22  23  24  25 ...
+ *          ^
+ *          |
+ *         Axis
  * 
  * Requested data must be carried back to square 1. Always take the shortest path: the Manhattan Distance between the
  * location of the data and square 1.
@@ -20,34 +23,63 @@
  * Your puzzle input is 312051
  */
 
-let table = [];
-let largestElement = 0;
+const _ = require('lodash');
 
-const getSideLength = (input) => {
+const getSteps = (input) => {
+	if (input < 1) {
+		throw new Error('No');
+	}
+
+	if (input === 1) {
+		return 0;
+	}
+
 	let sideLength = Math.ceil(Math.sqrt(input));
 	
 	if (sideLength % 2 === 0) {
 		sideLength++;
 	}
 
-	return sideLength;
-}
+	const largestElement = sideLength ** 2;
+	const corners = {};
 
-const stepsToCentre = (getSideLength(input) - 1) / 2;
-// return;
+	corners.bottomRight = largestElement,
+	corners.bottomLeft = largestElement - sideLength + 1,
+	corners.topLeft = corners.bottomLeft - sideLength + 1,
+	corners.topRight = corners.topLeft - sideLength + 1
 
-// for (let ring = 1; ring <= 280; ring++) {
-// 	const elementsInRing = ring === 1 ? 1 : (ring - 1) * 8;
+	let stepsToAxis = 0;
 
-// 	console.log(ring);
-// 	largestElement += elementsInRing
-// 	console.log('suurim', largestElement);
+	// such a dumb way
+	_.forEach(corners, corner => {
+		if (input === corner) {
+			stepsToAxis = (sideLength - 1) / 2;
+			return false;
+		}
 
-// 	//for (let element = 0; column < 3; column++) {
-// 		//table[row][]
-// 	//}
-// }
+		if (corner - input === (sideLength - 1) / 2) {
+			stepsToAxis = 0;
+			return false;
+		}
+
+		if (corner - input >= sideLength) {
+			// continue
+			return;
+		}
+
+		let axis = corner - (sideLength - 1) / 2;
+
+		stepsToAxis = Math.abs(axis - input);
+		return false;
+	});
+
+	return stepsToAxis + ((sideLength - 1) / 2);
+};
+
+const input = 312051;
+
+console.log(getSteps(input))
 
 module.exports = {
-	getSideLength
+	getSteps
 }
--- test/3-path-in-spiral.test.js
+++ test/3-path-in-spiral.test.js
@@ -1,12 +1,43 @@
-const { getSideLength } = require('../3-path-in-spiral');
+const { getSteps } = require('../3-path-in-spiral');
 
-describe('getSideLength', () => {
-	it('calculates side length of spiral matrix', () => {
-		expect(getSideLength(1)).toBe(1);
-		expect(getSideLength(2)).toBe(3);
-		expect(getSideLength(9)).toBe(3);
-		expect(getSideLength(21)).toBe(5);
-		expect(getSideLength(137)).toBe(13);
-		expect(getSideLength(312051)).toBe(559);
+describe('getSteps', () => {
+	it('throws an error in case input is less than 1', () => {
+		try {
+			getSteps(0);
+		} catch(error) {
+			expect(error).toEqual(new Error('No'));
+		}
+	});
+
+	it('1', () => {
+		expect(getSteps(1)).toBe(0);
+	});
+
+	it('9', () => {
+		expect(getSteps(9)).toBe(2);
+	});
+
+	it('8', () => {
+		expect(getSteps(8)).toBe(1);
+	});
+
+	it('2', () => {
+		expect(getSteps(2)).toBe(1);
+	});
+
+	it('17', () => {
+		expect(getSteps(17)).toBe(4);
+	});
+
+	it('14', () => {
+		expect(getSteps(14)).toBe(3);
+	});
+
+	it('12', () => {
+		expect(getSteps(12)).toBe(3);
+	});
+
+	it('1024', () => {
+		expect(getSteps(1024)).toBe(31);
 	});
 });
