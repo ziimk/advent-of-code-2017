df3cada8a47ff25a20024d9b976ca12233469b0c
Siim <siimkibus@gmail.com>
2017-12-04 23:37:45 +0200

    add day1 part 2, refactor

--- test/1-captcha.test.js
+++ test/1-captcha.test.js
@@ -5,25 +5,57 @@ describe('sequenceAdder()', () => {
 		it('1122', () => {
 			const input = '1122';
 
-			expect(sequenceAdder(input)).toBe(3);
+			expect(sequenceAdder(input, 1)).toBe(3);
 		});
 
 		it('11122', () => {
 			const input = '11122';
 		
-			expect(sequenceAdder(input)).toBe(4);
+			expect(sequenceAdder(input, 1)).toBe(4);
 		});
 
 		it('1111', () => {
 			const input = '1111';
 		
-			expect(sequenceAdder(input)).toBe(4);
+			expect(sequenceAdder(input, 1)).toBe(4);
 		});
+
+		it('does not add numbers from sequence if no matches', () => {
+			const input = '1234';
+	
+			expect(sequenceAdder(input, 1)).toBe(0);
+		})
 	});
 
-	it('does not add numbers from sequence if no matches', () => {
-		const input = '1234';
+	describe('adds number if it matches the one halfway around the circular list', () => {
+		it('1212', () => {
+			const input = '1212';
 
-		expect(sequenceAdder(input)).toBe(0);
-	})
+			expect(sequenceAdder(input, 2)).toBe(6);
+		});
+
+		it('1221', () => {
+			const input = '1221';
+		
+			expect(sequenceAdder(input, 2)).toBe(0);
+		});
+
+		it('123425', () => {
+			const input = '123425';
+		
+			expect(sequenceAdder(input, 3)).toBe(4);
+		});
+
+		it('123123', () => {
+			const input = '123123';
+		
+			expect(sequenceAdder(input, 3)).toBe(12);
+		});
+
+		it('12131415', () => {
+			const input = '12131415';
+		
+			expect(sequenceAdder(input, 4)).toBe(4);
+		});
+	});
 });
