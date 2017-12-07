9d0fa171544cf407e5071a25f094cdddd0ec90f2
Siim <siimkibus@gmail.com>
2017-12-07 22:56:36 +0200

    add day4 part 2

--- 4-passphrase.js
+++ 4-passphrase.js
@@ -6,6 +6,17 @@
  * aa bb cc dd ee is valid.
  * aa bb cc dd aa is not valid - the word aa appears more than once.
  * aa bb cc dd aaa is valid - aa and aaa count as different words.
+ * 
+ * --- Part Two ---
+ * Now, a valid passphrase must contain no two words that are anagrams of each other - that is, a passphrase is invalid
+ * if any word's letters can be rearranged to form any other word in the passphrase.
+ * 
+ * For example:
+ * abcde fghij is a valid passphrase.
+ * abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+ * a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+ * iiii oiii ooii oooi oooo is valid.
+ * oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
  */
 
 const fs = require('fs');
@@ -26,11 +37,12 @@ const readFile = () => {
  * @param {Array} row
  * @returns {bool}
  */
-const hasDuplicates = (row) => {
+const hasDuplicatesOrAnagrams = (row) => {
+	// ['hola', 'cdab', 'ohal'] => ['ahlo', 'abcd', 'ahlo']
+	const sortedWords = row.map(word => word.split('').sort().join(''));
+
 	// The Set object lets you store unique values of any type
-	const set = new Set();
-	
-	return row.some(word => set.size === set.add(word).size);
+	return (new Set(sortedWords)).size !== sortedWords.length;
 };
 
 const validatePassphrases = (input) => {
@@ -38,7 +50,7 @@ const validatePassphrases = (input) => {
 	let validCount = 0;
 
 	rows.forEach(row => {
-		if (!hasDuplicates(row.split(' '))) {
+		if (!hasDuplicatesOrAnagrams(row.split(' '))) {
 			validCount++;
 		}
 	});
@@ -51,6 +63,6 @@ readFile()
 	.catch(err => console.log(err));
 
 module.exports = {
-	hasDuplicates,
+	hasDuplicatesOrAnagrams,
 	validatePassphrases
 };
--- test/4-passphrase.test.js
+++ test/4-passphrase.test.js
@@ -1,16 +1,30 @@
-const { hasDuplicates, validatePassphrases } = require('../4-passphrase');
+const { hasDuplicatesOrAnagrams, validatePassphrases } = require('../4-passphrase');
 
-describe('hasDuplicates()', () => {
+describe('hasDuplicatesOrAnagrams()', () => {
 	it('returns true in case of duplicates', () => {
 		const input = 'aa bb cc dd aa'.split(' ');
 
-		expect(hasDuplicates(input)).toBe(true);
+		expect(hasDuplicatesOrAnagrams(input)).toBe(true);
 	});
 
 	it('returns false in case of no duplicates', () => {
 		const input = 'aa bb cc dd aaa'.split(' ');
 
-		expect(hasDuplicates(input)).toBe(false);
+		expect(hasDuplicatesOrAnagrams(input)).toBe(false);
+	});
+
+	it('returns false in case of no anagrams', () => {
+		const input = 'abcde fghij'.split(' ');
+		const input2 = 'a ab abc abd abf abj'.split(' ');
+
+		expect(hasDuplicatesOrAnagrams(input)).toBe(false);
+		expect(hasDuplicatesOrAnagrams(input2)).toBe(false);
+	});
+
+	it('returns true in case of anagrams', () => {
+		const input = 'abcde xyz ecdab'.split(' ');
+
+		expect(hasDuplicatesOrAnagrams(input)).toBe(true);
 	});
 });
 
